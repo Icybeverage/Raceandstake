3ea58da8d40cd5406dd08e84069fd3c1dca779be
Icybeverage <86324519+Icybeverage@users.noreply.github.com>
2024-07-28 07:28:45 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  endpoint: 'https://rest.unique.network/opal',
+  ownerSeed: '//Alice',
+  contractAddress: '5E5NhBNE1cAcQ6VXS79tdL6KehgXi6qKJ5KzzCy79NMKkCjd',
+  imagePartsFolder: './images',
+  imagePrefix: 'upgrade_',
+  imageWidth: 1024,
+  requiredCount: 10, // Number of NFTs to generate
+};
